4617c6a7587303cb2c9eddf1cc4873539b02e041
lera-2025 <lbrisovskaya@gmail.com>
2025-05-06 03:21:21 +0300

    Creation carousel

--- function-carousel.js
+++ function-carousel.js
@@ -1,3 +1,6 @@
+const element = document.getElementById("hhh"); 
+const rect = element.getBoundingClientRect(); 
+
 const images = ['img/Аверченко_Дюжина_ножей_в_спину_революции.png', 'img/Аверченко_Нечистая_сила.jpg', 'img/Аверченко_Осколки.jpg', 'img/Аверченко_Развороченный_муравейник.jpg'];
 const entry = ["Аркадий Аверченко. Дюжина ножей в спину революции. Париж, 1921","Аркадий Аверченко. Нечистая сила. Константинополь, 1921","Аркадий Аверченко. Осколки разбитого вдребезги. Москва; Ленинград, 1926","Аркадий Аверченко. Развороченный муравейник. Москва; Ленинград, 1927"]
 let img = document.querySelector ('img');
@@ -6,28 +9,33 @@ entr.className = "carousel-entry";
 const stopbtn = ("#carousel");
 let i = 0;
 
-window.setInterval (function(){
+const intervalId = setInterval (function(){
   img.src = images[i];
-  document.querySelectorAll('.carousel-entry').forEach((n, i) => n.textContent = entry[i]);
   entr.innerHTML = entry[i];
   document.body.append(entr);
   i++;
-   if (i == images.length) {i=0;}
-}, 1500);
+   if (i == images.length) {i=0};
+}, 3000);
 
-let downS = false;
-console.log("downS");
-stopbtn.mouseover(function() {
-  downS = true;
-
+const btnprev = document.querySelector('.button-prev');
+btnprev.addEventListener("click", ()=>{
+  // clearInterval(intervalId);
+  i = i-1;
+  if(i < 0){i = images.length - 1;}
+  img.src = images[i];
+  entr.innerHTML = entry[i];
+  document.body.append(entr);
 })
 
-function btn_next() {
-    imgbtn[j].classList.add('active');
-    j = (j + 1) % images.length;
-    imgbtn[j].classList.remove('active');
-console.log(imgbtn[j], j);
-}
+const btnnext = document.querySelector('.button-next');
+btnnext.addEventListener("click", ()=>{
+  // clearInterval(intervalId);
+  if(i < (images.length-1)) {i = i + 1;}
+  else {i = 0;}
+  img.src = images[i];
+  entr.innerHTML = entry[i];
+  document.body.append(entr);
+})
 
 
 
